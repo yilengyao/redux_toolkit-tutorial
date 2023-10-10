781872a68080f6b9e0a61005526bd9f42c2a76e2
YilengYao <yilengyao@gmail.com>
2023-10-10 14:08:34 -0700

    setup redux store add counter slice

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,17 @@
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./counterSlice";
+
+const Counter = ({ value, onIncrement, onDecrement }) => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+  return (
+    <section>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+    </section>
+  );
+};
+export default Counter;
