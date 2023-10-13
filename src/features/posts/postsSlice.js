d39a8bbebcac85ab3ce54c074d9b7dddb1f6d063
YilengYao <yilengyao@gmail.com>
2023-10-12 17:18:33 -0700

    pull post from typicode website

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -81,36 +81,38 @@ const postsSlice = createSlice({
       .addCase(fetchPosts.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
-      })
-      .addCase(postAdded.fulfilled, (state, action) => {
-        // Fix for API post IDs:
-        // Create sortedPosts & assigning the id
-        // would be not needed if the fake API
-        // returned accurate new post IDs
-        const sortedPosts = state.posts.sort((a, b) => {
-          if (a.id < b.id) return 1;
-          if (a.id > b.id) return -1;
-          return 0;
-        });
-        action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
-        // End  fix for fake API post IDs
-
-        action.payload.userId = Number(action.payload.userID);
-        action.payload.date = new Date().toISOString();
-        action.payload.reactions = {
-          thumbsUp: 0,
-          hooyah: 0,
-          heart: 0,
-          rocket: 0,
-          eyes: 0,
-        };
-        console.log(action.payload);
-        state.posts.push(action.payload);
       });
+    // .addCase(postAdded.fulfilled, (state, action) => {
+    //   // Fix for API post IDs:
+    //   // Create sortedPosts & assigning the id
+    //   // would be not needed if the fake API
+    //   // returned accurate new post IDs
+    //   const sortedPosts = state.posts.sort((a, b) => {
+    //     if (a.id < b.id) return 1;
+    //     if (a.id > b.id) return -1;
+    //     return 0;
+    //   });
+    //   action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
+    //   // End  fix for fake API post IDs
+    //
+    //   action.payload.userId = Number(action.payload.userID);
+    //   action.payload.date = new Date().toISOString();
+    //   action.payload.reactions = {
+    //     thumbsUp: 0,
+    //     hooyah: 0,
+    //     heart: 0,
+    //     rocket: 0,
+    //     eyes: 0,
+    //   };
+    //   console.log(action.payload);
+    //   state.posts.push(action.payload);
+    // });
   },
 });
 
 export const selectAllPosts = (state) => state.posts.posts;
+export const getPostsStatus = (state) => state.posts.status;
+export const getPostsError = (state) => state.posts.error;
 
 export const { postAdded, reactionAdded } = postsSlice.actions;
 
