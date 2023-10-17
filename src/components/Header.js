d4973e59f3dc76076daf9f8ec22cdd78cc6da620
YilengYao <yilengyao@gmail.com>
2023-10-17 12:14:26 -0700

    add count feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { increaseCount, getCount } from "../features/posts/postsSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const count = useSelector(getCount);
+
   return (
     <header className="Header">
       <h1>Redux Blog</h1>
@@ -16,6 +21,7 @@ const Header = () => {
             <Link to="/users">Users</Link>
           </li>
         </ul>
+        <button onClick={() => dispatch(increaseCount())}>{count}</button>
       </nav>
     </header>
   );
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,6 +1,5 @@
 import {
   createSlice,
-  nanoid,
   createAsyncThunk,
   createSelector,
 } from "@reduxjs/toolkit";
@@ -13,6 +12,7 @@ const initialState = {
   posts: [],
   status: "idle", //'idle' | 'loading' | 'succeeded' | 'failed'
   error: null,
+  count: 0,
 };
 
 export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
@@ -60,35 +60,15 @@ const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-    postAdded: {
-      reducer(state, action) {
-        state.posts.push(action.payload);
-      },
-      prepare(title, content, userId) {
-        return {
-          payload: {
-            id: nanoid(),
-            title,
-            content,
-            date: new Date().toISOString(),
-            userId,
-            reactions: {
-              thumbsUp: 0,
-              wow: 0,
-              heart: 0,
-              rocket: 0,
-              coffee: 0,
-            },
-          },
-        };
-      },
-    },
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload;
       const existingPost = state.posts.find((post) => post.id === postId);
       if (existingPost) {
         existingPost.reactions[reaction]++;
       }
+    },
+    increaseCount(state) {
+      state.count = state.count + 1;
     },
   },
   extraReducers(builder) {
@@ -170,6 +150,7 @@ const postsSlice = createSlice({
 export const selectAllPosts = (state) => state.posts.posts;
 export const getPostsStatus = (state) => state.posts.status;
 export const getPostsError = (state) => state.posts.error;
+export const getCount = (state) => state.posts.count;
 
 export const selectPostById = (state, postId) =>
   state.posts.posts.find((post) => post.id === postId);
@@ -179,6 +160,6 @@ export const selectPostsByUser = createSelector(
   (posts, userId) => posts.filter((post) => post.userId === userId),
 );
 
-export const { postAdded, reactionAdded } = postsSlice.actions;
+export const { increaseCount, reactionAdded } = postsSlice.actions;
 
 export default postsSlice.reducer;
